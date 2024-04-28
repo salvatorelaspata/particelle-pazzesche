941844e6d7ff49d335449d2e0530eb168b3e5ad2
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-04-28 15:27:00 +0200

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import glsl from 'vite-plugin-glsl'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/particelle-pazzesche/',
-  plugins: [react(), glsl()],
-})
