5117b158788b9a51f612f4d3f9ee6855eb776cfb
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-04-29 13:01:18 +0200

    adjustment ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   useParticel()
   return (
     <>
-      <div className='h-screen flex'>
+      {/* <div className='h-screen flex'>
         <h1 className="z-20 text-3xl font-bold underline text-white">
           Particelle pazzesche
         </h1>
-      </div>
+      </div> */}
       <canvas className='webgl absolute top-0 z-10'></canvas>
     </>
   )
